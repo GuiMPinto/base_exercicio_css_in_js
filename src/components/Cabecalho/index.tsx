278e882343235cc07ab07571e33fa8c3d228e594
PintoJR <guimoraespinto@gmail.com>
2025-05-11 20:28:52 -0300

    Tentando fazer o Cabeçalho

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,23 @@
-import styles from './Cabecalho.module.css'
+// ----  Sem Styled Component ---- //
+// import styles from './Cabecalho.module.css'
+
+// const Cabecalho = () => (
+//   <header className={styles.Cabecao}>
+//     <h1>EBAC Jobs</h1>
+//   </header>
+// )
+
+//export default Cabecalho
+
+// ----  Com Styled Component ---- //
+
+import { Cabeca } from './styles'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
+  <Cabeca>
+    {' '}
+    <h1>EBAC Jobs</h1>{' '}
+  </Cabeca>
 )
 
 export default Cabecalho
